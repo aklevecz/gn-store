8ecf6e001863cf6ae3029498ed17dd21eb11951c
Ariel Klevecz <arielklevecz@gmail.com>
2025-08-24 15:21:04 -0700

    cool hero working

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -121,7 +121,8 @@ function HeaderMenuMobileToggle() {
         }
       }}
     >
-      <h3>☰</h3>
+      {/* <h3>☰</h3> */}
+      <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M21 5.25H3M21 12H3m18 6.75H3"></path></svg>
     </button>
   );
 }
